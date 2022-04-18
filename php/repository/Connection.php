7a6c49c9d4a2eb7ca79b09c5ab2f53d9711bf70f
Arkymad <phmm2003@hotmail.com>
2022-04-18 18:14:09 -0300

    atualizações

--- php/repository/Connection.php
+++ php/repository/Connection.php
@@ -1,10 +1,10 @@
 <?php
     class Connection extends PDO {
 
-        const HOSTNAME = "HOST";
-        const USERNAME = "USER";
-        const PASSWORD = "PASS";
-        const SCHEMA = "DB";
+        const HOSTNAME = "ec2-44-199-143-43.compute-1.amazonaws.com";
+        const USERNAME = "stoscfnquepbpn";
+        const PASSWORD = "ad76f0773c0535207b1286d21c42b17650aaa080b60572ed588fab600a30e1cd";
+        const SCHEMA = "ddl8ca92v928poB";
         const PORT = 5432;
 
         private $conn;
